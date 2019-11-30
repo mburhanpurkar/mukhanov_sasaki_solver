85f83e5e4ad4e6c16fb7ef46253f1a2616552385
mburhanpurkar <maya.burhanpurkar@gmail.com>
2019-11-29 23:58:02 -0500

    updated heuristic for MS computation range

--- new_cosmology.py
+++ new_cosmology.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import time
 import math
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,7 +11,7 @@ from scipy.interpolate import interp1d as interp
 
 class InfModel:
     """All time-dependent quantities in this class are in terms of conformal time!"""
-    def __init__(self, V, dV, k, phi0=30, psi0=-0.00000001, a0=1, efold=30, niter=4000, t0=0):
+    def __init__(self, V, dV, k, phi0=30, psi0=-0.00000001, a0=1, efold=800, niter=2000, t0=0):
         self.V = V
         self.dV = dV
         self.init_params = [float(phi0), float(psi0)]  # solve for b(t) = log(a(t)) instead of a(t) as before
@@ -46,10 +47,11 @@ class InfModel:
 
             # Plot where inflation is happening!
             plt.plot(self.b, self.phi)
-            mask = np.where(4 * (self.psi**2 / 2) - 2 * self.V(self.phi) < 0, True, False)
-            binf = self.b[mask]
-            phiinf = self.phi[mask]
-            plt.plot(binf, phiinf, 'go', markersize=2)
+            # mask = np.where(4 * (self.psi**2 / 2) - 2 * self.V(self.phi) < 0, True, False)
+            # binf = self.b[mask]
+            # phiinf = self.phi[mask]
+            # plt.plot(binf, phiinf, 'go', markersize=2)
+            plt.plot(self.b, np.zeros(len(self.b)))
             plt.xlabel("Number of Efolds")
             plt.ylabel("Phi")
             plt.show()
@@ -69,7 +71,7 @@ class InfModel:
         zdoubleprime = np.gradient(zprime, self.b)
         x = np.exp(2 * self.b) * H * (zprime * H + Hprime * zprime + zdoubleprime * H) / z
 
-        bsmall = self.b[int(17.0 /self.efold * self.niter):int(28.0 /self.efold * self.niter)]
+        bsmall = self.b[int(11.0 /self.efold * self.niter):int(22.0 /self.efold * self.niter)]
         tmp = odeint(self.diffs2, self.mukhanov_params_re, bsmall, args=(x, H, self.k))
         vk_re = tmp[:, 0]
         dvk_re = tmp[:, 1]
@@ -95,10 +97,11 @@ class InfModel:
 
     def get_vt(self, plot_a=False, plot=False):
         self.get_a_phi(plot_a)
+        t1 = time.time()
         H = np.sqrt(self.V(self.phi) + self.psi**2 / 2)
         Hprime = np.gradient(H, self.b)
         y = H * np.exp(2 * self.b) * (Hprime + 2 * H)
-        bsmall = self.b[int(12.0 / self.efold * self.niter):int(21.0 / self.efold * self.niter)]
+        bsmall = self.b[int(19.0 / self.efold * self.niter):int(29.0 / self.efold * self.niter)]
         tmp = odeint(self.diffs3, self.mukhanov_params_re, bsmall, args=(y, H, self.k))
         vk_re = tmp[:, 0]
         dvk_re = tmp[:, 1]
@@ -108,12 +111,17 @@ class InfModel:
         dvk_im = tmp[:, 1]
 
         # #  RR = (H * v / dphi / a)**2 at horizon crossing
-        # i0 = (np.abs(H * self.a - self.k)).argmin()
-        # print self.t[i0]
+        i0 = (np.abs(H[int(19.0 / self.efold * self.niter):int(29.0 / self.efold * self.niter)] * np.exp(bsmall) - self.k)).argmin()
+        print bsmall[i0]
+        R = (H[int(19.0 / self.efold * self.niter):int(29.0 / self.efold * self.niter)] / self.psi[int(19.0 / self.efold * self.niter):int(29.0 / self.efold * self.niter)]) * np.sqrt(vk_im**2 + vk_re**2) / np.exp(bsmall)
+        print R[i0]
+        t2 = time.time()
+
+        print "runtime (mins)", (t2 - t1) / 60.0
         # # print("D^2_R(k=" + str(k) + ") = " + str((H[i0] * vk[i0] / self.dphi[i0] / self.a[i0])**2))
         #
         if plot:
-            plt.plot(vk_re, vk_im)
+            plt.plot(vk_im, vk_re)
         #     plt.plot(self.b, H)
         #     plt.plot(self.b, np.zeros(len(self.b)))
         #     # plt.plot(self.t, vk_re**2 + vk_im**2)
@@ -163,14 +171,17 @@ class InfModel:
 
 def V(phi):
     # return np.exp(2 * phi)
-    return 0.1 * phi**2
+    return 0.1 * phi**(2 / 3)
 
 
 def dV(phi):
-    # return 2 * np.exp(2 * phi)
-    return 2 * 0.1 * phi
+    return 0.1 * 2 / 3 * phi**(-1 / 3)
 
 
-model = InfModel(V, dV, math.exp(24))
-# model.get_a_phi(plot=True)
-model.get_vt(True, True)
+model = InfModel(V, dV, math.exp(25), phi0=30)
+model.get_a_phi(plot=True)
+model = InfModel(V, dV, math.exp(25), phi0=40)
+model.get_a_phi(plot=True)
+model = InfModel(V, dV, math.exp(25), phi0=50)
+model.get_a_phi(plot=True)
+# model.get_vt(True, True)
